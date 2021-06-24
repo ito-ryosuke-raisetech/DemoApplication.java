b295c3b7ee2d142c46dfe5862012603cd40b0a27
ito-ryosuke-raisetech <85591560+ito-ryosuke-raisetech@users.noreply.github.com>
2021-06-24 10:44:03 +0900

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -12,21 +12,5 @@ public class DemoApplication {
 	}
 
 }
-//HelloController.java
 
-package com.example.demo;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class HelloController {
-	
-	
-	 @RequestMapping(value="/hello")
-	 private String hello() {
-		 return"index.html";
-	 }
-
-}
 
